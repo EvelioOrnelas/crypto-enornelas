17504b844e3380285a6eabf540a706576894ba2e
Evelio Ornelas <evelioornelas@outlook.com>
2021-12-08 23:05:05 -0700

    added profile window

--- FinalApp.py
+++ FinalApp.py
@@ -11,6 +11,13 @@ def make_win1():
                 [sg.Button('Get User Info', key='userInfo', enable_events=True), sg.Button('Get Account Info', key='Account', enable_events=True)],
                 [sg.Output(size=(110, 30), background_color='black', text_color='white')]]
     return sg.Window('Coinbase GUI', layout, resizable=True, finalize=True)
+def make_win2():
+    layout = [  [sg.Image(data=png_data, key="-ArtistAvatarIMG-")],
+                [sg.Text('Name: ', size=(15, 1)), sg.Text(name)],
+                [sg.Text('Email: ', size=(15, 1)), sg.Text(email)],
+                [sg.Text('Currency: ', size=(15, 1)), sg.Text(currency)],
+                [sg.Text('State: ', size=(15, 1)), sg.Text(state)]]
+    return sg.Window('Profile', layout, resizable=True, finalize=True)
 
 def getConfig(file):
     fd = open(file,"r")
@@ -37,12 +44,33 @@ while True:
             assert(len(coinbasesecret)!=0)
             assert(len(coinbasekey)!=0)
             client = Client(api_key=coinbasekey, api_secret=coinbasesecret)
-            user = client.get_current_user()
-            print(user)
+            #user = client.get_current_user()
+            #print(user)
         except:
             print("Invalid coinbase configuration file")
             time.sleep(3)
             break
+    if event == 'userInfo':
+        user = client.get_current_user()
+        avatar = user.avatar_url
+        name = user.name
+        email = user.email
+        currency = user.native_currency
+        state = user.state
+        jpg_data = (
+        cloudscraper.create_scraper(
+            browser={"browser": "firefox", "platform": "windows", "mobile": False}
+        )
+        .get(avatar)
+        .content
+        )
+        pil_image = Image.open(io.BytesIO(jpg_data))
+        png_bio = io.BytesIO()
+        pil_image.save(png_bio, format="PNG")
+        png_data = png_bio.getvalue()
+        window2 = make_win2()  
+        #user = client.get_current_user()
+        #print(user)
     if event == sg.WIN_CLOSED or event == 'Exit': 
         window.close()
         if window == window2:       # if closing win 2, mark as closed
