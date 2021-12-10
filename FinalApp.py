36157ac31bf9f8c408058f49b96b68fae7f26004
Evelio Ornelas <evelioornelas@outlook.com>
2021-12-09 23:26:49 -0700

    added feature to get account info && successfully displayed account info

--- FinalApp.py
+++ FinalApp.py
@@ -3,16 +3,20 @@ import configparser, pathlib, time, requests, urllib, cloudscraper, io
 from pathlib import Path
 from coinbase.wallet.client import Client
 from PIL import Image
+import json
 
 sg.theme('DarkAmber')
 def make_win1():
     layout = [  #[sg.Menu(menu_def, tearoff=True, pad=(10, 10))],
-                [sg.Text('Config file: ', size=(15, 1)), sg.InputText(key='_FILEBROWSE_', enable_events=True), sg.FileBrowse(target='_FILEBROWSE_')],
-                [sg.Button('Get User Info', key='userInfo', enable_events=True), sg.Button('Get Account Info', key='Account', enable_events=True)],
-                [sg.Output(size=(110, 30), background_color='black', text_color='white')]]
+                [sg.Text('Config file: ', size=(15, 1)), sg.InputText(key='_FILEBROWSE_', enable_events=True), sg.FileBrowse(target='_FILEBROWSE_'), sg.Button('Get User Info', key='userInfo', enable_events=True)],
+                [sg.Column([[sg.Text('Portfolio', font=("Verdana", 30))]], justification='center')],
+                [sg.Text('Your balance')],
+                [sg.Text('', key='AccountName', enable_events=True, font=('Verdana', 21))],
+                [sg.Text('', key='Bitcoin', enable_events=True), sg.Text('', key='balAmount', enable_events=True)],
+                [sg.Output(size=(90, 1), background_color='black', text_color='white')]]
     return sg.Window('Coinbase GUI', layout, resizable=True, finalize=True)
 def make_win2():
-    layout = [  [sg.Image(data=png_data, key="-ArtistAvatarIMG-")],
+    layout = [  [sg.Column([[sg.Image(data=png_data, key="-ArtistAvatarIMG-")]], justification='center')],
                 [sg.Text('Name: ', size=(15, 1)), sg.Text(name)],
                 [sg.Text('Email: ', size=(15, 1)), sg.Text(email)],
                 [sg.Text('Currency: ', size=(15, 1)), sg.Text(currency)],
@@ -31,12 +35,18 @@ def getConfig(file):
     )
     return config['default']
 
+# url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/BTC_Logo.svg/1200px-BTC_Logo.svg.png'
+# response = requests.get(url, stream=True)
+# response.raw.decode_content = True
+# img_box = response.raw.read()
+# img_box.thumbnail((50, 50))
+
 window1, window2 = make_win1(), None
 while True:
     window, event, values = sg.read_all_windows()
-    infile = str(values['_FILEBROWSE_'])
     if(event == '_FILEBROWSE_'):
         try:
+            infile = str(values['_FILEBROWSE_'])
             config=getConfig(infile)
             print('Got configuration file!')
             coinbasesecret=str(config.get('coinbasesecret',''))
@@ -44,39 +54,53 @@ while True:
             assert(len(coinbasesecret)!=0)
             assert(len(coinbasekey)!=0)
             client = Client(api_key=coinbasekey, api_secret=coinbasesecret)
-            #user = client.get_current_user()
-            #print(user)
+            auser = client.get_accounts()
+            json_dump = json.dumps(auser)
+            json_object = json.loads(json_dump)
+            amount = ''
+            aname = ''
+            acurrency = ''
+            for each in json_object['data']:
+                if each['name'] == 'BTC Wallet':
+                    #print(each['name'])
+                    amount = each['native_balance']['amount']
+                    aname = each['balance']['amount']
+                    acurrency = each['balance']['currency']
+            window.Element('AccountName').Update('$' + amount)
+            window.Element('Bitcoin').Update('Bitcoin')
+            window.Element('balAmount').Update(aname + ' ' + acurrency)
         except:
             print("Invalid coinbase configuration file")
             time.sleep(3)
             break
     if event == 'userInfo':
-        user = client.get_current_user()
-        avatar = user.avatar_url
-        name = user.name
-        email = user.email
-        currency = user.native_currency
-        state = user.state
-        jpg_data = (
-        cloudscraper.create_scraper(
-            browser={"browser": "firefox", "platform": "windows", "mobile": False}
-        )
-        .get(avatar)
-        .content
-        )
-        pil_image = Image.open(io.BytesIO(jpg_data))
-        png_bio = io.BytesIO()
-        pil_image.save(png_bio, format="PNG")
-        png_data = png_bio.getvalue()
-        window2 = make_win2()  
-        #user = client.get_current_user()
-        #print(user)
-    if event == sg.WIN_CLOSED or event == 'Exit': 
+        try:
+            user = client.get_current_user()
+            avatar = user.avatar_url
+            name = user.name
+            email = user.email
+            currency = user.native_currency
+            state = user.state
+            jpg_data = (
+            cloudscraper.create_scraper(
+                browser={"browser": "firefox", "platform": "windows", "mobile": False}
+            )
+            .get(avatar)
+            .content
+            )
+            pil_image = Image.open(io.BytesIO(jpg_data))
+            png_bio = io.BytesIO()
+            pil_image.save(png_bio, format="PNG")
+            png_data = png_bio.getvalue()
+            window2 = make_win2()
+        except:
+            print('Choose configuration file first!')
+    if event == sg.WIN_CLOSED or event == 'Exit':
         window.close()
         if window == window2:       # if closing win 2, mark as closed
             window2 = None
         elif window == window1:     # if closing win 1, exit program
             break
-    if event in (window2, 'Exit'):
+    if event == 'Close':
         window2.close()
 window.close()
